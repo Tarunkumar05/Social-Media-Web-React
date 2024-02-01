a8f2bc8e13dcd2b674532fcb5edf62d1292cd45b
Tarun Kumar Bairwa <101545470+Tarunkumar05@users.noreply.github.com>
2024-02-01 17:54:50 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import "./login.scss"
+import "./Login.scss"
 import { Link } from 'react-router-dom';
 import { AuthContext } from "../../context/authContext";
 
